b8babb1bd9adcfc74c877a138eff81bae7adc2e6
wangzhizhou <824219521@qq.com>
2019-09-19 21:50:10 +0800

    refine

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -3,11 +3,12 @@
 
 sudo apt-get update
 
-sudo apt-get install -y libjpeg-dev
+sudo apt-get install -y libjpeg-dev libpng-dev libfreetype6-dev 
 sudo apt-get install -y python-virtualenv
 sudo apt-get install -y python3
 sudo apt-get install -y python3-dev
 sudo apt-get install -y mysql-server
+sudo apt-get install -y redis-server
 
 virtualenv py3 -p python3
 
@@ -21,6 +22,19 @@ fi
 mkdir -p logs/
 
 # config.py 
+
+# config mysql
+# su - 
+# mysql
+# create database bbs;
+# create user joker@'%' identified by 'minecraft';
+# grant all priviledges `bbs`.* to joker@'%';
+# flush priviledges;
+
+# config redis
+
+sudo systemctl restart mysql.service
+sudo systemctl restart redis-server.service
 
 python runserver.py initdb
 python runserver.py create-index
